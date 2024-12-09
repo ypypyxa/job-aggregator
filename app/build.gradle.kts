ee5186ee1921140f605eb961587eb9baaea431ac
Anar <dtalievanar@gmail.com>
2024-12-09 12:57:52 +0300

    возврат обратно

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,13 +88,4 @@ dependencies {
     ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-
-
-
-
-
-
-
-
-
 }
