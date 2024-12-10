b940eb797e5789507b973266fab15362d2fb9c04
ypypyxa <axypypy@yandex.ru>
2024-12-11 01:28:50 +0300

    Исправление замечаний бота
    Удалены еще комментарии TODO ((
    Изменен порядок импортов

--- app/src/main/java/ru/practicum/android/diploma/favorites/ui/FavoritesViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/ui/FavoritesViewModel.kt
@@ -3,5 +3,5 @@ package ru.practicum.android.diploma.favorites.ui
 import androidx.lifecycle.ViewModel
 
 class FavoritesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
 }
--- app/src/main/java/ru/practicum/android/diploma/vacancy/ui/VacancyViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/ui/VacancyViewModel.kt
@@ -3,5 +3,5 @@ package ru.practicum.android.diploma.vacancy.ui
 import androidx.lifecycle.ViewModel
 
 class VacancyViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
 }
