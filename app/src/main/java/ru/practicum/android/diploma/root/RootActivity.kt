192414148d7e0ba2396ccef0867ebff55830520c
ypypyxa <axypypy@yandex.ru>
2024-12-12 00:07:21 +0300

    Реализация BottomMenu
    bottom_navigation_menu.xml:
    Исправлена опечатка и название домашнего фрагмента
    
    root_navigation_graph.xml:
    Исправлена опечатка
    
    RootActivity.kt:
    Реализован переход между экранами с помощью BottomMenu

--- app/src/main/java/ru/practicum/android/diploma/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/RootActivity.kt
@@ -2,7 +2,11 @@ package ru.practicum.android.diploma.root
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
 
@@ -10,6 +14,29 @@ class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.vacancyFragment -> {
+                    bottomNavigationView.visibility = View.VISIBLE
+                }
+                R.id.favoritesFragment -> {
+                    bottomNavigationView.visibility = View.VISIBLE
+                }
+                R.id.teamFragment -> {
+                    bottomNavigationView.visibility = View.VISIBLE
+                }
+                else -> {
+                    bottomNavigationView.visibility = View.GONE
+                }
+            }
+        }
 
         // Логируем токен для проверки
         val accessToken = BuildConfig.HH_ACCESS_TOKEN
