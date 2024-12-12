c7cb24fc84868c123d3583c2a4699e03e3d331b3
ypypyxa <axypypy@yandex.ru>
2024-12-12 23:38:06 +0300

    Исправление замечаний бота
    DetailsFragment.kt:
    Перенесен атрибут на новую строку

--- app/src/main/java/ru/practicum/android/diploma/vacancy/details/ui/DetailsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/details/ui/DetailsFragment.kt
@@ -21,7 +21,8 @@ class DetailsFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_details, container, false)
