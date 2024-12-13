4f4c3a05985b22d2818f5958cab4ef1953253fa9
Anar <dtalievanar@gmail.com>
2024-12-13 18:59:34 +0300

    рефакторинг DI

--- app/src/main/java/ru/practicum/android/diploma/app/di/AppModule.kt
+++
@@ -1,8 +0,0 @@
-package ru.practicum.android.diploma.app.di
-
-import com.google.gson.Gson
-import org.koin.dsl.module
-
-val appModule = module {
-    single { Gson() }
-}
--- app/src/main/java/ru/practicum/android/diploma/favorites/di/FavoritesModule.kt
+++
@@ -1,9 +0,0 @@
-package ru.practicum.android.diploma.favorites.di
-
-import org.koin.core.module.dsl.viewModel
-import org.koin.dsl.module
-import ru.practicum.android.diploma.favorites.ui.FavoritesViewModel
-
-val favoritesModule = module {
-    viewModel { FavoritesViewModel() }
-}
