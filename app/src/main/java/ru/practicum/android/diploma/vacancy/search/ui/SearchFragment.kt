ddbdee1b9a06f2100e22642785f9f70af8904542
Anar <dtalievanar@gmail.com>
2024-12-20 18:38:50 +0300

    начало

--- app/src/main/java/ru/practicum/android/diploma/vacancy/search/ui/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/search/ui/SearchFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
@@ -20,7 +21,8 @@ class SearchFragment : Fragment() {
     private val viewModel: SearchViewModel by viewModel()
     private val vacancyAdapter by lazy {
         VacancyAdapter(emptyList()) { vacancy ->
-            // Обработка нажатия на вакансию при необходимости
+            val action = SearchFragmentDirections.actionSearchFragmentToDetailsFragment(vacancy)
+            findNavController().navigate(action)
         }
     }
 
