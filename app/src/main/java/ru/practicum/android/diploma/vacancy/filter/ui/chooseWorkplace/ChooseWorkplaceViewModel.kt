60ae9440184d04a6eedf22d612ee745e6750e36b
Anar <dtalievanar@gmail.com>
2024-12-23 14:10:07 +0300

    Исправления детект

--- app/src/main/java/ru/practicum/android/diploma/vacancy/filter/ui/chooseWorkplace/ChooseWorkplaceViewModel.kt
+++
@@ -1,7 +0,0 @@
-package ru.practicum.android.diploma.vacancy.filter.ui.chooseWorkplace
-
-import androidx.lifecycle.ViewModel
-
-class ChooseWorkplaceViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/ru/practicum/android/diploma/vacancy/filter/ui/chooseworkplace/ChooseWorkplaceFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/filter/ui/chooseworkplace/ChooseWorkplaceFragment.kt
@@ -1,11 +1,11 @@
-package ru.practicum.android.diploma.vacancy.filter.ui.chooseWorkplace
+package ru.practicum.android.diploma.vacancy.filter.ui.chooseworkplace
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import ru.practicum.android.diploma.R
 
 class ChooseWorkplaceFragment : Fragment() {
@@ -19,11 +19,11 @@ class ChooseWorkplaceFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // TODO: Use the ViewModel
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_choose_workplace, container, false)
