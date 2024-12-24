b2f9830dcf0b47236f6861d15e7e38e0d9cb88f0
Polinochka <lrtqzx@gmail.com>
2024-12-24 19:50:09 +0500

    Поиск после "Нет интернета" и отображение количества найденных вакансий Upd

--- app/src/main/java/ru/practicum/android/diploma/common/utils/SetVacancyCountText.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/utils/SetVacancyCountText.kt
@@ -4,8 +4,8 @@ import android.widget.TextView
 
 fun TextView.setVacancyCountText(count: Int) {
     val text = when {
-        count % 10 == 1 && count % 100 != 11 -> "Найдена $count вакансия"
-        count % 10 in 2..4 && (count % 100 !in 12..14) -> "Найдено $count вакансии"
+        count % 10 == 1 && count % 100 !in 12..14 -> "Найдена $count вакансия"
+        count % 10 in 2..4 && count % 100 !in 12..14 -> "Найдено $count вакансии"
         else -> "Найдено $count вакансий"
     }
     this.text = text
