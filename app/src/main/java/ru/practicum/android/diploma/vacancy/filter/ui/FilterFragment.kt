3799a6b5df96c3d3580575443e1f7207577c4241
Anar <dtalievanar@gmail.com>
2024-12-26 21:24:35 +0300

    Детект

--- app/src/main/java/ru/practicum/android/diploma/vacancy/filter/ui/FilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/filter/ui/FilterFragment.kt
@@ -8,9 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import androidx.navigation.fragment.navArgs
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
